d3bbbeb49ecf497e31bc471d26b55c7ad1a812ff
benson-tseng <benson010603@gmail.com>
2020-11-01 18:02:11 +0800

    first push

---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <v-text-field v-model="pokemon" label="pokemon"></v-text-field>
+    <v-btn @click="search">search</v-btn>
+    <v-card>
+      <v-img width="100px" :src="pic"></v-img>
+      <v-card-title v-if="test">{{ res.species.name }}</v-card-title>
+      <v-card-text v-if="test">id:{{ res.id }}</v-card-text>
+      <v-card-text v-if="test">
+        type:
+        <span v-for="(item, i) in res.types" :key="i">
+          {{ item.type.name }}
+        </span>
+      </v-card-text>
+      <v-card-text v-if="test">
+        weight:
+        <span>{{ res.weight / 10 }}kg</span>
+      </v-card-text>
+      <v-card-text v-if="test">
+        height:
+        <span>{{ res.height / 10 }}m</span>
+      </v-card-text>
+      <v-card-text v-if="test">
+        hp:
+        <span>{{ res.stats[0]['base_stat'] }}</span>
+      </v-card-text>
+      <v-card-text v-if="test">
+        attack:
+        <span>{{ res.stats[1]['base_stat'] }}</span>
+      </v-card-text>
+      <v-card-text v-if="test">
+        defense:
+        <span>{{ res.stats[2]['base_stat'] }}</span>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pokemon: undefined,
+      res: undefined,
+      pic: undefined,
+      test: false,
+    }
+  },
+  methods: {
+    async search() {
+      const poke = this.pokemon.toLowerCase()
+      this.res = await this.$axios.$get(
+        `https://pokeapi.co/api/v2/pokemon/${poke}`
+      )
+      this.pic = `https://pokeres.bastionbot.org/images/pokemon/${this.res.id}.png`
+      this.test = true
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/plz.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-container fluid>
+    <v-row align="center">
+      <v-col class="d-flex" cols="12" sm="6">
+        <v-select :items="items" label="Standard" dense></v-select>
+      </v-col>
+
+      <v-col class="d-flex" cols="12" sm="6">
+        <v-select :items="items" filled label="Filled style" dense></v-select>
+      </v-col>
+
+      <v-col class="d-flex" cols="12" sm="6">
+        <v-select
+          :items="items"
+          label="Outlined style"
+          dense
+          outlined
+        ></v-select>
+      </v-col>
+
+      <v-col class="d-flex" cols="12" sm="6">
+        <v-select :items="items" label="Solo field" dense solo></v-select>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+      attributes: [
+        '一般',
+        '火',
+        '水',
+        '草',
+        '電',
+        '冰',
+        '格鬥',
+        '飛行',
+        '毒',
+        '地面',
+        '岩石',
+        '蟲',
+        '幽靈',
+        '鋼',
+        '超能力',
+        '龍',
+        '惡',
+        '妖精',
+      ],
+      attributesApi: [
+        1,
+        10,
+        11,
+        12,
+        13,
+        15,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        14,
+        16,
+        17,
+        18,
+      ],
+    }
+  },
+  methods: {},
+}
+</script>
+
+<style></style>
